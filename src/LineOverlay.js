aea6230c7358fbc2844ba935bab2246d87dc9b11
Jared Bowns <soundboyzent@gmail.com>
2018-03-25 20:38:15 -0400

    stash my build, let's use different test data

--- src/LineOverlay.js
+++
@@ -1,5 +0,0 @@
-import React, {Component} from 'react';
-import {setParameters} from 'luma.gl';
-import DeckGL, {LineLayer, ScatterplotLayer} from 'deck.gl';
-
-const MAPBOX_TOKEN = 'pk.eyJ1IjoiamJvd256aW5vIiwiYSI6ImNqZWp6ZjBhMTNydnQydmxudzRqN3R2bW4ifQ.CquyG6X0xN3PfoeEDbi64A';
